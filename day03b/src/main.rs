0d565d6be1cad65b52c2fcec5508f01a9812ebc7
Ken Zhou <kendotzhou@gmail.com>
2022-12-09 14:53:25 -0500

    day03

--- day03b/src/main.rs
+++ day03b/src/main.rs
@@ -6,36 +6,40 @@ fn main() {
 
     let file = std::fs::File::open(filename).unwrap();
 
-    let lines = std::io::BufRead::lines(std::io::BufReader::new(file));
+    let mut lines = std::io::BufRead::lines(std::io::BufReader::new(file));
 
     let mut sum: u32 = 0;
 
-    for line in lines {
-        if let Ok(line) = line {
-            let line = line.trim();
-            let comp1 = line.get(0..line.len() / 2).unwrap();
-            let comp2 = line.get(line.len() / 2..line.len()).unwrap();
-
+    loop {
+        if let (Some(Ok(sack1)), Some(Ok(sack2)), Some(Ok(sack3))) = (lines.next(), lines.next(), lines.next()) {
             let mut present = HashMap::<char, bool>::new();
 
-            for c in comp1.chars() {
+            for c in sack1.chars() {
                 present.insert(c, true);
             }
 
-            for c in comp2.bytes() {
-                if present.get(&c.into()) == Some(&true) {
+            for c in sack2.chars() {
+                if present.get(&c) != Some(&true) {
+                    present.insert(c, false);
+                }
+            }
+
+            for c in sack3.bytes() {
+                let as_char = char::from_u32(c as u32).unwrap();
+                if let (Some(_), Some(_)) = (sack1.find(as_char), sack2.find(as_char)) {
                     let value:u32 = if c >= 97 {
                         // Lowercase
                         (c - 97 + 1).into()
                     } else {
                         (c - 65 + 27).into()
                     };
-                    // println!("{:?}, {}", char::from_u32(c as u32), value);
 
                     sum += value;
                     break;
                 }
             }
+        } else {
+            break;
         }
     }
 
